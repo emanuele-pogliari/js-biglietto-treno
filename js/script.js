bfef75adf52c813f3697ff5a61a964bd52165f3b
emanuele-pogliari <manu.pogliari@gmail.com>
2024-01-28 20:44:56 +0100

    commented code and general optimization. Exercise completed with extras and bonuses.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 let kilometers;
 let age;
 
+// a timeout of 200ms that will load js code after loading the page
 setTimeout(function () {
 
     kilometers = prompt("Inserisci chilometri da percorrere");
@@ -26,18 +27,23 @@ setTimeout(function () {
         document.getElementById("price").innerHTML = `${priceTicket.toFixed(2)} €`;
         document.getElementsByClassName("data")[0].classList.toggle("visible");
     } else {
+        // create 2 variables that will have value equals to one when the condition are true.
         let check_age;
         let check_km;
         document.getElementsByClassName("data_error")[0].classList.toggle("visible");
         if (isNaN(age) || (age < 0 && age > 110) || (age % 1 != 0)) {
             document.getElementsByClassName("text_error")[0].innerHTML = "Hai inserito un valore non valido per l'età. Ricarica la pagina con un click sul bottone qui sotto";
             check_age = 1;
-        } if (isNaN(kilometers) || kilometers < 0 || (kilometers % 1 != 0)) {
+        }
+        if (isNaN(kilometers) || kilometers < 0 || (kilometers % 1 != 0)) {
             document.getElementsByClassName("text_error")[0].innerHTML = "Hai inserito un valore non valido per i chilometri. Ricarica la pagina con un click sul bottone qui sotto";
             check_km = 1;
-        } if (check_age + check_km === 2) {
+        }
+        // if the sum of check variable are two, both condition are true, user insert some invalid information, therefore, the condition below will inform user of a double error input
+        if (check_age + check_km === 2) {
             document.getElementsByClassName("text_error")[0].innerHTML = "Hai inserito un valore non valido per ETA' e KM. Ricarica la pagina con un click sul bottone qui sotto";
         }
+        // this function will wait for a click from user on the reload button. When this button will be clicked, the page will reload.
         document.getElementsByClassName("btn_reload")[0].addEventListener("click", function () {
             window.location.reload();
         })
