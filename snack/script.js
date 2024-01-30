f4cade9aba55ab2b81265c593c6680e12b40d38e
emanuele-pogliari <manu.pogliari@gmail.com>
2024-01-30 14:49:07 +0100

    fix apps and added some style

--- snack/script.js
+++ snack/script.js
@@ -1,68 +1,83 @@
 let menu = document.getElementById("menu");
-let converter = document.getElementById("converter");
+let converterApp = document.getElementById("converter");
+let avarageApp = document.getElementById("average");
+let roSciPa = document.getElementById("ro-sci-pa");
 
 // function that if called will start the game
 
 function rockScissorPaper() {
     menu.style.display = "none";
+    roSciPa.style.display = "block"
 
-    // document.getElementById("sceltagioco").style.display = "none";
-    // document.getElementById("pincopallo1").style.display = "block";
-    let userChoice = prompt("Inserisci una scelta: 1: Sasso, 2: Forbice, 3: Carta").toLowerCase();
+    setTimeout(function () {
+        let userChoice = prompt("Inserisci una scelta: 1: Sasso, 2: Forbice, 3: Carta").toLowerCase();
 
-    while (userChoice != "sasso" && userChoice != "forbice" && userChoice != "carta") {
-        userChoice = prompt("Scelta non valida. Inserisci : 1: Sasso, 2: Forbice, 3: Carta").toLowerCase();
-    }
 
-    let user_val;
+        while (userChoice != "sasso" && userChoice != "forbice" && userChoice != "carta") {
+            userChoice = prompt("Scelta non valida. Inserisci : 1: Sasso, 2: Forbice, 3: Carta").toLowerCase();
+        }
 
-    if (userChoice === "sasso") {
-        user_val = 1;
-    } else if (userChoice === "forbice") {
-        user_val = 2;
-    } else {
-        user_val = 3;
-    }
+        let computerVal;
+        let computerChoice = Math.floor(Math.random() * 3 + 1);
 
-    let computerChoice = Math.floor(Math.random() * 3 + 1);
+        if (computerChoice === 1) {
+            computerVal = "sasso";
+        }
+        else if (computerChoice === 2) {
+            computerVal = "forbice";
+        }
+        else {
+            computerVal = "carta";
+        }
 
-    if (user_val === computerChoice) {
-        document.getElementById("result").innerHTML = "pareggio";
-    } else {
-        if (user_val === 1 && computerChoice === 2 || user_val === 2 && computerChoice === 3 || user_val === 3 && computerChoice === 1) {
+        document.getElementById("user-choice").innerHTML = userChoice;
+        document.getElementById("computer-choice").innerHTML = computerVal;
+
+        if (userChoice === computerVal) {
+            document.getElementById("result").innerHTML = "pareggio";
+        } else if (userChoice == "sasso" && computerVal == "forbice" || userChoice == "forbice" && computerVal == "carta" || userChoice == "carta" && computerVal == "sasso") {
             document.getElementById("result").innerHTML = "vittoria utente";
         }
         else {
             document.getElementById("result").innerHTML = "vittoria computer";
         }
-    }
-    return 0;
+    }, 200)
 }
 
 // function that if called will start the game
 function avarage() {
     menu.style.display = "none";
-    let num_1 = Number(prompt("Inserisci primo numero"));
-    let num_2 = Number(prompt("Inserisci secondo numero"));
-    let num_3 = Number(prompt("Inserisci terzo numero"));
+    avarageApp.style.display = "block"
 
-    let average = (num_1 + num_2 + num_3) / 3;
+    setTimeout(function () {
+        let num_1 = Number(prompt("Inserisci primo numero"));
+        let num_2 = Number(prompt("Inserisci secondo numero"));
+        let num_3 = Number(prompt("Inserisci terzo numero"));
 
-    document.getElementById("result").innerHTML = `La media tra ${num_1}, ${num_2} e ${num_3} è ${average}`;
+        let average = (num_1 + num_2 + num_3) / 3;
+        document.getElementById("numero-1").innerHTML = num_1;
+        document.getElementById("numero-2").innerHTML = num_2;
+        document.getElementById("numero-3").innerHTML = num_3;
+        document.getElementById("result_average").innerHTML = `La media tra i numeri è: ${average}`;
+    }, 200)
 }
 
 // function that if called will start the game
 function converter() {
     menu.style.display = "none";
-    let userDegrees = Number(prompt(" Inserisci i gradi da convertire"));
+    converterApp.style.display = "block";
+    setTimeout(function () {
+        let userDegrees = Number(prompt(" Inserisci i gradi da convertire"));
 
-    while (isNaN(userDegrees)) {
-        userDegrees = prompt("Errore. Inserisci i gradi da convertire");
-    }
+        while (isNaN(userDegrees)) {
+            userDegrees = prompt("Errore. Inserisci i gradi da convertire");
+        }
 
-    let conversion = (userDegrees * 9 / 5) + 32;
+        let conversion = (userDegrees * 9 / 5) + 32;
 
-    document.getElementById("result").innerHTML = `La coversione di ${userDegrees} °C è ${conversion} °F`;
+        document.getElementById("user-degrees").innerHTML = `${userDegrees} °C`;
+        document.getElementById("converted").innerHTML = `${conversion} °F`;
+    }, 200)
 }
 
 // function that if called will reload the page
